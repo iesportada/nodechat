b8f0ba34ae209d70d15327d6b11d2689951cec70
iesportada <eliseo.moreno@gmail.com>
2024-01-11 00:11:37 +0100

    Cambio en el título del chat

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Chat' });
+  res.render('index', { title: 'Express Chat v2' });
 });
 
 module.exports = router;
